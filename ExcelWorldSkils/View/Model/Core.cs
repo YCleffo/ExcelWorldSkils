b724fb5a77997a7a7631903752bb54a0490ad850
emk7online <emk7online@gmail.com>
2023-01-17 11:33:39 +0500

    Добавьте файлы проекта.

---
+++ ExcelWorldSkils/View/Model/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWorldSkils.View.Model
+{
+    public class Core
+    {
+        public excelEntities context = new excelEntities();
+    }
+}
---
+++ ExcelWorldSkils/View/Pages/ListStudentPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ExcelWorldSkils.View.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcelWorldSkils.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListStudentPage.xaml
+    /// </summary>
+    public partial class ListStudentPage : Page
+    {
+        Core db = new Core();
+        public ListStudentPage()
+        {
+            InitializeComponent();
+            ListDataGrid.ItemsSource = db.context.Students.ToList();
+        }
+    }
+}
