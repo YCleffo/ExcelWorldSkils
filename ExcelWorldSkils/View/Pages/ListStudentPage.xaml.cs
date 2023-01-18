6843f566c7f309d97f3b9f48451b4b256bbe182c
emk7online <emk7online@gmail.com>
2023-01-18 14:37:14 +0500

    ready

--- ExcelWorldSkils/View/Pages/ListStudentPage.xaml.cs
+++ ExcelWorldSkils/View/Pages/ListStudentPage.xaml.cs
@@ -18,7 +18,8 @@ namespace ExcelWorldSkils.View.Pages
         public ListStudentPage()
         {
             InitializeComponent();
-            ListDataGrid.ItemsSource = db.context.Students.ToList();
+            arrayStudents = db.context.Students.ToList();
+            ListDataGrid.ItemsSource = arrayStudents;
             ComboBoxSort.ItemsSource = db.context.Groups.ToList();
             ComboBoxSort.DisplayMemberPath = "NameGroup";
             ComboBoxSort.SelectedValuePath = "IdGroup";
@@ -47,10 +48,12 @@ namespace ExcelWorldSkils.View.Pages
             /*создаем лист*/
             Excel.Worksheet worksheet = workbook.ActiveSheet;
 
-            worksheet.StandardWidth = 20;
-            worksheet.Columns.ColumnWidth = 20;
+            //worksheet.StandardWidth = 30;
+            //worksheet.Columns.ColumnWidth = 30;
 
-            
+            worksheet.Name = "Отчет студентов";
+
+            int rowIndex = 2;
 
             worksheet.Cells[1][1] = "ФИО";
             worksheet.Cells[2][1] = "Группа";
@@ -58,8 +61,23 @@ namespace ExcelWorldSkils.View.Pages
             worksheet.Cells[4][1] = "Форма обучения";
             worksheet.Cells[5][1] = "Год поступления";
 
-            
+            int i = Convert.ToInt32(ComboBoxSort.SelectedValue);
+            if (i != 0) arrayStudents = db.context.Students.Where(x => x.IdGroup == i).ToList();
+            foreach (var item in arrayStudents)
+            {
+                worksheet.Cells[1][rowIndex] = item.FIO;
+                worksheet.Cells[2][rowIndex] = item.Groups.NameGroup;
+                worksheet.Cells[3][rowIndex] = item.Professions.NameProfession;
+                worksheet.Cells[4][rowIndex] = item.FormTime.Name;
+                worksheet.Cells[5][rowIndex] = item.YearAdd.Year;
 
+                rowIndex++;
+               
+            }
+            Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[5][rowIndex - 1]];
+            rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle = rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle = rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle = rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                 rangeBorders.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle = rangeBorders.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle = Excel.XlLineStyle.xlContinuous;
+            worksheet.Columns.AutoFit();
         }
     }
 }
