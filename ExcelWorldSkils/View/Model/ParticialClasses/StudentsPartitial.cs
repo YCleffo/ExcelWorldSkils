23cd75e1dc4d714f954e306a62733927647ef81b
emk7online <emk7online@gmail.com>
2023-01-18 13:28:47 +0500

    fd

---
+++ ExcelWorldSkils/View/Model/ParticialClasses/StudentsPartitial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWorldSkils.View.Model
+{
+    public partial class Students
+    {
+        public string FIO
+        {
+            get
+            {
+                return FiestName + " " + LastName + " " + PatronomicName;
+            }
+        }
+
+    }
+}
--- ExcelWorldSkils/View/Pages/ListStudentPage.xaml.cs
+++ ExcelWorldSkils/View/Pages/ListStudentPage.xaml.cs
@@ -2,17 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelWorldSkils.View.Pages
 {
@@ -22,10 +14,52 @@ namespace ExcelWorldSkils.View.Pages
     public partial class ListStudentPage : Page
     {
         Core db = new Core();
+        List<Students> arrayStudents;
         public ListStudentPage()
         {
             InitializeComponent();
             ListDataGrid.ItemsSource = db.context.Students.ToList();
+            ComboBoxSort.ItemsSource = db.context.Groups.ToList();
+            ComboBoxSort.DisplayMemberPath = "NameGroup";
+            ComboBoxSort.SelectedValuePath = "IdGroup";
+        }
+
+        private void ComboBoxSortSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idGroup = Convert.ToInt32(ComboBoxSort.SelectedValue);
+            arrayStudents = db.context.Students.Where(x => x.IdGroup == idGroup).ToList();
+            ListDataGrid.ItemsSource = arrayStudents;
+        }
+
+        private void ConclusionBtnClick(object sender, RoutedEventArgs e)
+        {
+            Excel.Application aplication = new Excel.Application();
+            aplication.Visible = true;
+
+
+            /*количество листов*/
+
+            aplication.SheetsInNewWorkbook = 1;
+
+            /*добавляем рабочую книгу*/
+            Excel.Workbook workbook = aplication.Workbooks.Add(Type.Missing);
+
+            /*создаем лист*/
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            worksheet.StandardWidth = 20;
+            worksheet.Columns.ColumnWidth = 20;
+
+            
+
+            worksheet.Cells[1][1] = "ФИО";
+            worksheet.Cells[2][1] = "Группа";
+            worksheet.Cells[3][1] = "Специальность";
+            worksheet.Cells[4][1] = "Форма обучения";
+            worksheet.Cells[5][1] = "Год поступления";
+
+            
+
         }
     }
 }
